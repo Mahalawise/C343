774275b479bc0686bb719da9ca888a911f98ac5e
Mahala Wise <ma.wise0367@gmail.com>
2022-12-05 12:21:49 -0500

    Done

--- Huffman Coding/HuffmanTree.java
+++ Huffman Coding/HuffmanTree.java
@@ -1,4 +1,6 @@
 import java.util.Comparator;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * TODO: Complete the implementation of this class.
@@ -44,9 +46,7 @@ public class HuffmanTree {
             }
         }
     }
-
     private Node root;
-
     /**
      * TODO
      *
@@ -54,7 +54,29 @@ public class HuffmanTree {
      * alphabet using the algorithm described in lecture.
      */
     public HuffmanTree(FrequencyTable charFreqs) {
-
+        //creating a forest of nodes comparator
+        Heap<Node> forest = new Heap<Node>(new Compares());
+        //get the characters of the frequency table
+        if (!charFreqs.isEmpty()) {
+            for (Map.Entry<Character,Integer> chars : charFreqs.entrySet()) {
+                forest.push(new Node(chars.getValue(),chars.getKey()));
+            }
+            ///creating the huffman tree
+            Node combines = null;
+            //root node
+            if(forest.size()==1){
+                Node n = forest.pop();
+                this.root = new Node(n.priority,n.key);
+            }
+            while(forest.size() != 1){
+                Node a = forest.pop();
+                Node b = forest.pop();
+                combines = new Node(a.priority+b.priority, a,b);
+                forest.push(combines);
+            }
+            this.root=combines;
+            System.out.println(forest.toString());
+        }
     }
 
     /**
@@ -65,7 +87,33 @@ public class HuffmanTree {
      * @throws DecodeException if bits does not match a character in the tree.
      */
     public char decodeChar(String bits) {
-        return '\0';
+        //setting node to root
+        Node curr = this.root;
+        if(bits.isEmpty() && curr!=null){
+            return curr.key;
+        }
+        for(char c: bits.toCharArray()){
+            if(curr==null){
+                throw new DecodeException(bits);
+            }
+
+            if (curr.isLeaf() && curr != this.root){
+                return curr.key;
+            }
+            //left path
+            if(c == '0'){
+                curr = curr.left;
+            }
+            //right path
+            if(c == 1){
+                curr=curr.right;
+            }
+        }
+
+        if(curr.isLeaf()&& curr != this.root){
+            return curr.key;
+        }
+        throw new DecodeException(bits);
     }
 
     /**
@@ -79,6 +127,33 @@ public class HuffmanTree {
      * @throws EncodeException if the character does not appear in the tree.
      */
     public String lookup(char ch) {
+        //recursion
+        if(this.root.key==ch){
+            return "";
+        }
+        String character = recursion(this.root,ch,"");
+        if(character!=""){
+            return character;
+        }
+        throw new EncodeException(ch);
+    }
+
+
+    public String recursion(Node n,char c, String s){
+        if(n ==null){
+            return "";
+        } else if(n.key == c){
+            return s;
+        } else {
+            String left = recursion(n.left, c, s + "0");
+            String right = recursion(n.right, c, s + "1");
+            if (!Objects.equals(right, "")){
+                return left;
+            }
+            if(!Objects.equals(left,"")){
+                return right;
+            }
+        }
         return "";
     }
 
@@ -87,6 +162,14 @@ public class HuffmanTree {
             return "";
         else
             return root.toString();
+    }
+
+    ///must create a comparator class in order to use the heap class
+    public static class Compares implements Comparator<Node>{
+        ///creating a min heap
+        public int compare(Node o1, Node o2) {
+            return o1.priority- o2.priority;
+        }
     }
 }
 
